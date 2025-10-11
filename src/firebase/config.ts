8ef7a488e55aae9b0a0136ca683da3426dbbb419
Hezron Choolo <choolohezron@gmail.com>
2025-10-11 16:11:49 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9176892725-60858",
+  "appId": "1:834916416668:web:be6e430428816eb9b14159",
+  "apiKey": "AIzaSyB5wUyztcCqqRsx52JRxrgLzanSJFhpKsM",
+  "authDomain": "studio-9176892725-60858.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "834916416668"
+};
