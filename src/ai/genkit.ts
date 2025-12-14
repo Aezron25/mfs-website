873bb7ed6ccadda815ca4f7be68f7585a7afaee6
Hezron Choolo <choolohezron@gmail.com>
2025-12-14 18:06:20 +0000

    update it to react version 19.2.1

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -3,13 +3,12 @@
  */
 
 import { genkit } from 'genkit';
-import { googleAI } from '@genkit-ai/googleai';
+import { googleAI } from '@genkit-ai/google-genai';
 
 export const ai = genkit({
   plugins: [
     googleAI({
       apiVersion: 'v1beta',
-      apiKey: process.env.GOOGLE_API_KEY,
     }),
   ],
   logLevel: 'debug',
